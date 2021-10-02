88234c2d3b7933de4bebbfa08a44f38e068a351d
Dilip Singh <dilipsingh1306@gmail.com>
2021-10-02 13:35:43 +0530

    first data

---
+++ src/test/java/com/eclasses/account/UserAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eclasses.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
