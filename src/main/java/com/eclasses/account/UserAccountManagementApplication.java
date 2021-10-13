e7c53e7520ac13431d4c0b30088c3d11666f1abf
Dilip Singh <dilipsingh1306@gmail.com>
2021-10-13 08:28:26 +0530

    Feign client

--- src/main/java/com/eclasses/account/UserAccountManagementApplication.java
+++ src/main/java/com/eclasses/account/UserAccountManagementApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import feign.Logger;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +15,12 @@ public class UserAccountManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserAccountManagementApplication.class, args);
+	}
+	
+	@Bean
+	Logger.Level geignLoggerLevel(){
+		
+		return Logger.Level.FULL;
 	}
 
 }
